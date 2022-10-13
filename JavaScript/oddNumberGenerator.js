ece13d217ad4b85bdabf7054b259129be86df86c
ruhin <ruhinm3@gmail.com>
2022-10-13 22:50:52 +0600

    odd number generator in js

---
+++ JavaScript/oddNumberGenerator.js
@@ -0,0 +1,14 @@
+/**
+ * github username : me-ruhin 
+*/
+
+const generateOddNumber = (limit)=>{
+    for(let i = 1;i<=limit;i++){
+        if(i%2 == 1){
+            console.log(i)
+        }
+    }
+}
+
+generateOddNumber(100)
+
